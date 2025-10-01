c77c54b226c917e0b14cccb7462fc9e52e623c0b
2270366 <2270366@LTIN511242.cts.com>
2025-10-01 21:27:12 +0530

    Added the third system.out.println using the sts ui...

--- src/com/practice/HelloWorld.java
+++ src/com/practice/HelloWorld.java
@@ -8,7 +8,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is just hello world!!");
 		System.out.println("Added one more print statement");
+		System.out.println("This is added in the master branch");
 	}
+	
 }
 
 
