a177e01b0488e61fc14dad65f68481b5f2e3d066
Kapil Dev Sharma <71621562+kapildevsharma02@users.noreply.github.com>
2025-10-01 21:21:01 +0530

    Adding the add function......
    Adding the add functionality in the HelloWorld class...

--- src/com/practice/HelloWorld.java
+++ src/com/practice/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.practice;
 
 public class HelloWorld {
-
+	public static int add(int a,int b){
+		return a+b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("This is just hello world!!");
 		System.out.println("Added one more print statement");
 	}
 }
+
 
