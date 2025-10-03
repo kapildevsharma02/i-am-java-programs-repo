c209dad41c27ac5ce920055e0d9541c1690891b9
Kapil Dev Sharma <71621562+kapildevsharma02@users.noreply.github.com>
2025-10-03 11:57:45 +0530

    hecking the pr status

--- src/com/practice/Divide.java
+++ src/com/practice/Divide.java
@@ -4,6 +4,7 @@ public class Divide{
 
 	public static void main(String[] args) {
 		System.out.println("adding the divide functionality");
+		System.out.println("Adding the yash system.println in release r4 to check the pr");
 	}
 }
 
